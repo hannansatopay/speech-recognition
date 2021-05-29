cfae157aed234c2d7aefa344f531ac00ac87a555
Hannan Satopay <sathannan@hotmail.com>
2021-05-29 12:53:00 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,10 @@
+import { registerPlugin } from '@capacitor/core';
+
+import type { SpeechRecognitionPlugin } from './definitions';
+
+const SpeechRecognition = registerPlugin<SpeechRecognitionPlugin>('SpeechRecognition', {
+  web: () => import('./web').then(m => new m.SpeechRecognitionWeb()),
+});
+
 export * from './definitions';
-export * from './web';
+export { SpeechRecognition };
