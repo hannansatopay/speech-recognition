68b5184025fb3a551fec72dde9b656997f0c86ac
Hannan Satopay <sathannan@hotmail.com>
2021-06-02 18:39:44 +0530

    Refresh

---
+++ dist/esm/index.d.ts
@@ -0,0 +1,5 @@
+import type { SpeechRecognitionPlugin } from './definitions';
+declare const SpeechRecognition: SpeechRecognitionPlugin;
+export * from './definitions';
+export { SpeechRecognition };
+
